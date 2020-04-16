74b73bf36e9940c49b02977c31c51aa567aa6693
unknown <jatinmodi001@gmail.com>
2020-04-17 01:15:45 +0530

    removing bugs

--- app.py
+++ app.py
@@ -383,4 +383,5 @@ def index():
 
 #---------------------------------------------------------------------------------------------
 if __name__ == "__main__":
-	app.run(debug=True)
+	port = int(os.environ.get("PORT", 5000))
+	app.run(debug=True,port=port)
