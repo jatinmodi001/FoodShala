063b8cdf20b7f4b88985e39f058228267c589e8d
unknown <jatinmodi001@gmail.com>
2020-04-17 14:37:06 +0530

    removing bugs

--- app.py
+++ app.py
@@ -12,6 +12,8 @@ app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///FoodShala.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+#------------------------------------DATABASE MODELS-------------------------------
+
 class Items(db.Model):
 	__tablename__ = 'items'
 	restaurantId = db.Column(db.Integer,ForeignKey('restaurants.id'))
@@ -87,15 +89,17 @@ def home():
 @app.route("/menu",methods=['GET'])
 def menu():
 	items = Items.query.order_by(Items.dishtype).all()
+	veg = True
 	if checkSession():
 		user = Users.query.filter_by(email=session['email']).first()
-
+		veg = user.veg
 		# if user is non veg then sorting non veg items according to that
 
 		if not user.veg:
 			items.sort(key=get_key)
-	return render_template("menu.html",items=items,user=checkSession(),userVeg=user.veg)
+	return render_template("menu.html",items=items,user=checkSession(),userVeg=veg)
 
+# function for sorting the items array according to veg/nonveg
 def get_key(obj):
 	return obj.veg
 
